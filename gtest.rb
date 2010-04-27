63a13849a9cf83f01173ffb06fe50234301f4109
Zach Aysan <zachaysan@gmail.com>
2010-04-26 21:07:10 -0400

    bringing in the main files

---
+++ gtest.rb
@@ -0,0 +1,119 @@
+
+require 'chi_prob.rb'
+
+class GTestException < Exception
+end
+
+class GTest
+  attr_accessor :min_successes, :yates
+  
+  #split_groups.form? => split_group[:a_split_group] = [trails, successes]
+  def initialize(split_groups, min_successes=10.0, yates=:on)
+    raise "GTest expects a hash of split groups" unless split_groups.is_a? Hash
+    raise GTestException.new("need at least two split tests") if split_groups.size < 2
+    @min_successes = min_successes
+    @split_groups = split_groups
+    @yates = yates
+  end
+  def assure_format
+    @split_groups.each do |split_group, measures|
+      raise GTestException.new("GTest expects a nested array of measures") unless measures.is_a? Array 
+    end
+  end
+  def find_best_performing
+    assure_format
+    current_group, best_group_so_far = 0.0, 0.0
+    @split_groups.each do |split_group, measure|
+      current_group = measure[1].to_f/measure[0]
+      if current_group > best_group_so_far
+        best_group_so_far = current_group
+        @best_performing_group = split_group
+      end
+    end
+    @best_performing_group
+  end
+  def compare_split_groups
+    # Returns a hash of results: split_group_results[split_group_name] = [g_stat, confidence]
+    find_best_performing
+    @split_group_results = {}
+    g_stat, confidence = 0.0, 0.0
+    @split_groups.each do |split_group, measure|
+      g_stat, confidence = calculate_g(split_group, @best_performing_group) unless split_group == @best_performing_group
+      @split_group_results[split_group] = [g_stat, confidence]
+    end
+    return @split_group_results
+  end
+  def ensure_minimum(winning_successes, winning_failures, other_successes, other_failures)
+    # setting min failures to min successes is on purpose. 
+    # I don't think it is worth confusing people over the difference
+    # because 99.9999% of the time people will worry about 
+    # min_successes over min_failures
+    @min_failures = @min_successes 
+    custom_g_stat = nil
+    custom_other_successes = other_successes
+    if winning_successes < @min_successes or winning_failures < @min_failures 
+      custom_g_stat = 0
+    elsif other_failures < @min_failures
+      custom_g_stat = 0
+    elsif other_successes < @min_successes \
+      and (other_successes + other_failures) > (@min_successes + @min_failures) \
+      and (@min_successes / (other_failures + other_successes)) < \
+      winning_successes / (winning_successes + winning_failures)
+      custom_other_successes = @min_successes.to_f
+    end
+    return custom_g_stat, custom_other_successes
+  end
+  def calculate_g(other_group, winning_group)
+    winning_trials = @split_groups[winning_group][0].to_f
+    winning_successes = @split_groups[winning_group][1].to_f
+    winning_failures = winning_trials - winning_successes
+
+    other_trials = @split_groups[other_group][0].to_f
+    other_successes = @split_groups[other_group][1].to_f
+    other_failures = other_trials - other_successes
+
+    g_statistic, other_successes = ensure_minimum(winning_successes, winning_failures, other_successes, other_failures)
+    other_failures = other_trials - other_successes
+    
+    total_trials = winning_trials + other_trials
+    total_successes = winning_successes + other_successes
+    total_failures = winning_failures + other_failures
+
+    expected_winning_successes = winning_trials.to_f * total_successes / total_trials
+    expected_winning_failures = winning_trials.to_f * total_failures / total_trials
+    expected_other_successes = other_trials.to_f * total_successes / total_trials
+    expected_other_failures = other_trials.to_f * total_failures / total_trials
+    
+    winning_successes = yates_correct(winning_successes, expected_winning_successes)
+    winning_failures = yates_correct(winning_failures, expected_winning_failures)
+    other_successes = yates_correct(other_successes, expected_other_successes)
+    other_failures = yates_correct(other_failures, expected_other_failures)
+
+    if g_statistic.nil?
+      g_statistic = 2 * ( winning_successes * Math::log(winning_successes / expected_winning_successes) + \
+                          winning_failures * Math::log(winning_failures / expected_winning_failures) +    \
+                          other_successes * Math::log(other_successes / expected_other_successes) +      \
+                          other_failures * Math::log(other_failures / expected_other_failures) )
+    end
+
+    chi_prob = Chi_Prob.new
+    confidence = 1 - (chi_prob.calculate_new_x g_statistic)
+    return g_statistic, confidence
+  end
+  def yates_correct(actual, expected)
+    if @yates == :on
+      if expected + 0.5 < actual
+        actual -= 0.5
+      elsif expected - 0.5 > actual
+        actual += 0.5
+      else
+        actual = expected
+      end
+    end
+    actual
+  end
+end
+
+#aliases for forgetful users
+class Gtest < GTest
+end
---
+++ gtest_spec.rb
@@ -0,0 +1,181 @@
+require 'gtest'
+module GTestHelperMethods
+  def generate_gtest_scenario(number_of_groups)
+    split_test_hash = {}
+    for i in 1..number_of_groups do
+      split_test_hash["split_group_#{i}"] = [900 + rand(200), 15 + rand(30)]
+    end
+    split_test_hash
+  end
+end
+
+describe GTest do
+  include GTestHelperMethods
+  before(:all) do
+    sample_gtest = generate_gtest_scenario(2)
+    @gtest = GTest.new sample_gtest
+  end
+  it "should not blow up on init given a simple ab test" do
+    sample_gtest = generate_gtest_scenario(2)
+    small_gtest = GTest.new sample_gtest
+  end
+  it "should be able to load multiple test groups" do
+    sample_gtest = generate_gtest_scenario(5)
+    large_gtest = GTest.new sample_gtest
+  end
+  it "should allow a minimum number of successes to be set, with a default of 10" do
+    @gtest.min_successes = 30
+  end
+  it "should only allow nested arrays as a measure" do
+    @gtest.find_best_performing
+    sample_incorrect_gtest = {"first_split_group" => "200:15", "second_split_group" => "205:15"}
+    incorrect_gtest = GTest.new sample_incorrect_gtest
+    lambda { incorrect_gtest.assure_format }.should raise_error(GTestException)
+  end
+  it "should correctly find the best performing split group" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100], 
+      :middle_split_group  => [1000, 125]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.find_best_performing.should == :winning_split_group
+  end
+  it "should allow the Yates continuity correction to be turned off or on" do
+    @gtest.yates = :off
+    @gtest.yates.should == :off
+  end
+  
+  it "should return the correct G-test statistic with yates off and only two groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :off
+    split_results = predetermined_gtest.compare_split_groups
+    gstat, confidence = split_results[:loosing_split_group]
+    rounded_gstat = (gstat*10_000).round.to_f / 10_000
+    rounded_gstat.should == 11.4965
+  end
+  it "should return the correct confidence with yates off and only two groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :off
+    split_results = predetermined_gtest.compare_split_groups
+    gstat, confidence = split_results[:loosing_split_group]
+    rounded_confidence = (confidence*10_000).round.to_f / 10_000
+    rounded_confidence.should == 0.9993
+  end
+  it "should return the correct G-test statistic with yates on and only two groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :on
+    
+    split_results = predetermined_gtest.compare_split_groups
+    gstat, confidence = split_results[:loosing_split_group]
+    rounded_gstat = (gstat*10_000).round.to_f / 10_000
+    rounded_gstat.should == 11.0386
+  end
+  it "should return the correct confidence with yates on and only two groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :on
+    split_results = predetermined_gtest.compare_split_groups
+    gstat, confidence = split_results[:loosing_split_group]
+    rounded_confidence = (confidence*10_000).round.to_f / 10_000
+    rounded_confidence.should == 0.9991
+  end
+  it "should return the correct G-test statistics with yates on and multiple groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100], 
+      :middle_split_group  => [1000, 125]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :on
+    split_results = predetermined_gtest.compare_split_groups
+    array_of_g_stats = [(split_results[:loosing_split_group][0]*10_000).round.to_f / 10_000, 
+                        (split_results[:middle_split_group][0]*10_000).round.to_f / 10_000]
+    array_of_g_stats.should == [11.0386, 2.4311]
+  end
+  it "should return the correct G-test statistics with yates on and multiple groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100], 
+      :middle_split_group  => [1000, 125]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :on
+    split_results = predetermined_gtest.compare_split_groups
+    array_of_confidences = [(split_results[:loosing_split_group][1]*10_000).round.to_f / 10_000, 
+                        (split_results[:middle_split_group][1]*10_000).round.to_f / 10_000]
+    array_of_confidences.should == [0.9991, 0.8811]
+  end
+  it "should return the correct G-test statistics with yates off and multiple groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100], 
+      :middle_split_group  => [1000, 125]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :off
+    split_results = predetermined_gtest.compare_split_groups
+    array_of_g_stats = [(split_results[:loosing_split_group][0]*10_000).round.to_f / 10_000, 
+                        (split_results[:middle_split_group][0]*10_000).round.to_f / 10_000]
+    array_of_g_stats.should == [11.4965, 2.6382]
+  end
+  it "should return the correct G-test statistics with yates on and multiple groups" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 150], 
+      :loosing_split_group => [1000, 100], 
+      :middle_split_group  => [1000, 125]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    predetermined_gtest.yates = :off
+    split_results = predetermined_gtest.compare_split_groups
+    array_of_confidences = [(split_results[:loosing_split_group][1]*10_000).round.to_f / 10_000, 
+                        (split_results[:middle_split_group][1]*10_000).round.to_f / 10_000]
+    array_of_confidences.should == [0.9993, 0.8957]
+  end
+  it "should return adjusted G-test statistics for small success sizes" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 25], 
+      :loosing_split_group => [1000, 5], #should be adjusted to [1000, minimum]
+      :middle_split_group  => [1000, 15]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    split_results = predetermined_gtest.compare_split_groups
+    array_of_g_stats = [(split_results[:loosing_split_group][0]*10_000).round.to_f / 10_000, 
+                        (split_results[:middle_split_group][0]*10_000).round.to_f / 10_000]
+    array_of_g_stats.should == [5.8595, 2.0838]
+
+  end
+  it "should return adjusted confidences for small success sizes" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [1000, 25], 
+      :loosing_split_group => [1000, 5], #should be adjusted to [1000, minimum]
+      :middle_split_group  => [1000, 15]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    split_results = predetermined_gtest.compare_split_groups
+    array_of_confidences = [(split_results[:loosing_split_group][1]*10_000).round.to_f / 10_000, 
+                            (split_results[:middle_split_group][1]*10_000).round.to_f / 10_000]
+    array_of_confidences.should == [0.9845, 0.8511]
+
+  end
+  it "should return a 0 G-test statistic for really small tests" do
+    sample_predetermined_gtest = {
+      :winning_split_group => [11, 5], 
+      :loosing_split_group => [12, 3], #should be adjusted to [1000, minimum]
+      :middle_split_group  => [13, 4]}
+    predetermined_gtest = GTest.new sample_predetermined_gtest
+    split_results = predetermined_gtest.compare_split_groups
+    array_of_confidences = [(split_results[:loosing_split_group][1]*10_000).round.to_f / 10_000, 
+                            (split_results[:middle_split_group][1]*10_000).round.to_f / 10_000]
+    array_of_confidences.should == [0.0, 0.0]
+  end
+  it "should not allow the creation of a split test with less than two split groups" do
+    sample_gtest = generate_gtest_scenario(1)
+    lambda { broken_gtest = GTest.new sample_gtest }.should raise_error(GTestException)  
+  end
+end
+
