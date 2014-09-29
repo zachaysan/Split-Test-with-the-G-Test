7b09c7cc7c6a8b109056f8906a6f44b5faeca430
Zach Aysan <zachaysan@gmail.com>
2014-09-29 10:47:39 -0400

    reformat

--- chi_prob.rb
+++ chi_prob.rb
@@ -1,13 +1,15 @@
 require "statistics2"
 
 class Chi_Prob
-def initialize
-end
-def calculate_new_x(x)
-  x = 15.0 if x > 15
-  @answer = 1.0 - Statistics2.chi2dist(1,x)
-end
-def get_answer
-  return @answer
-end
+  def initialize
+  end
+  
+  def calculate_new_x(x)
+    x = 15.0 if x > 15
+    @answer = 1.0 - Statistics2.chi2dist(1,x)
+  end
+  
+  def get_answer
+    return @answer
+  end
 end
